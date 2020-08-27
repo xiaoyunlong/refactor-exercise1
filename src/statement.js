b9f8a2b9d894b1fd682f6788ccab93a1793772ee
xiaoyunlong <1758217010@qq.com>
2020-08-27 15:36:15 +0800

    [exercise][Lebron] refactor: extract 2 method calculateTragedyAmount() and calculateTragedyAmount()

--- src/statement.js
+++ src/statement.js
@@ -2,46 +2,73 @@ function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
+
   const format = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
   }).format;
+
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
+
     let thisAmount = getAmount(play,perf);
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
+//
+//    volumeCredits = getVolumeCredits(play,perf);
+ volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
     if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+
     //print line for this order
     result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
+
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits \n`;
   return result;
 }
 
+//function getVolumeCredits(play,perf){
+//    // add volume credits
+//    let volumeCredits = 0;
+//    volumeCredits += Math.max(perf.audience - 30, 0);
+//    // add extra credit for every ten comedy attendees
+//    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+//
+//    return volumeCredits;
+//}
+
 function getAmount(play,perf){
     let thisAmount = 0;
     switch (play.type) {
       case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-        }
+        thisAmount = calculateTragedyAmount(perf.audience);
         break;
       case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
+        thisAmount = calculateComedyAmount(perf.audience);
         break;
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
+    return thisAmount;
+}
+
+function calculateTragedyAmount(audienceNumber){
+    let thisAmount = 40000;
+    if (audienceNumber > 30) {
+        thisAmount += 1000 * (audienceNumber - 30);
+    }
+    return thisAmount;
+}
+
+function calculateComedyAmount(audienceNumber){
+    let thisAmount = 30000;
+    if (audienceNumber > 20) {
+       thisAmount += 10000 + 500 * (audienceNumber - 20);
+    }
+    thisAmount += 300 * audienceNumber;
     return thisAmount;
 }
 
